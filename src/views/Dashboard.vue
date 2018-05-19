ed2a011b0abf684cd14caebbb3c6ab34c67dc759
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-05-19 13:27:11 +0200

    Removed unused vue components

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Schema id="1" name="test"/>
+    <Schema id="2" name="test 2"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Schema from '@/components/Schema.vue'
+
+export default {
+  name: 'dashboard',
+  components: {
+    Schema
+  }
+}
+</script>
