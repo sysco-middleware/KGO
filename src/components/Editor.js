c3d60b1ea8bce88bc6fb249c3225b4b0cea3690c
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-05-30 00:33:12 +0200

    feat: ace editor

---
+++ src/components/Editor.js
@@ -0,0 +1,14 @@
+import Ace from '@sysco-middleware/vue-ace'
+import 'brace/mode/json'
+
+export default {
+  extends: Ace,
+  mounted () {
+    this.createEditor({
+      showPrintMargin: false,
+      tabSize: 2,
+      useSoftTabs: true,
+      mode: 'ace/mode/json'
+    })
+  }
+}
