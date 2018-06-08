039d18336a6a679c2f16afd8bcc1ade8d92da66c
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-06-08 08:38:49 +0200

    api

--- src/api/schema-registry-api.js
+++ src/api/schema-registry-api.js
@@ -21,7 +21,8 @@ export default {
     })
   },
   fetchSubjectVersions (subjectName) {
-    return axios.get(`${ROOT_URL}/subjects/${subjectName}/versions`, defaultHeaders)
+    return axios.get(`${ROOT_URL}/subjects/${subjectName}/versions`,
+        defaultHeaders)
   },
   registerNewSchemaVersion (subjectName, schema) {
     return axios.post(`${ROOT_URL}/subjects/${subjectName}/versions`, {
@@ -30,13 +31,54 @@ export default {
     })
   },
   fetchSchema (subjectName, versionId) {
-    return axios.get(`${ROOT_URL}/subjects/${subjectName}/versions/${versionId}`, defaultHeaders)
+    return axios.get(
+        `${ROOT_URL}/subjects/${subjectName}/versions/${versionId}`,
+        defaultHeaders)
   },
   // TODO: only dev
   deleteSchemaVersion (subjectName, versiondId) {
-    return axios.delete(`${ROOT_URL}/subjects/${subjectName}/versions/${versiondId}`, defaultHeaders)
+    return axios.delete(
+        `${ROOT_URL}/subjects/${subjectName}/versions/${versiondId}`,
+        defaultHeaders)
   },
   fetchAvroSchema (subjectName, versionId) {
-    return axios.get(`${ROOT_URL}/subjects/${subjectName}/versions/${versionId}/schema`, defaultHeaders)
+    return axios.get(
+        `${ROOT_URL}/subjects/${subjectName}/versions/${versionId}/schema`,
+        defaultHeaders)
+  },
+  testSchema (subjectName, versionId, schema) {
+    return axios.post(`${ROOT_URL}/compatibility/subjects/${subjectName}/versions/${versionId}`, {
+          headers: defaultHeaders.headers,
+          body: schema
+        }
+    )
+  },
+  /**
+   * NONE, FULL, FORWARD, BACKWARD, FULL_TRANSITIVE, FORWARD_TRANSITIVE, BACKWARD_TRANSITIVE
+   */
+  updateCompatibility (compatibilityLevel) {
+    return axios.put(`${ROOT_URL}/config`, {
+      headers: defaultHeaders.headers,
+      body: {
+        'compatibility': `\"${compatibilityLevel}\"`
+      }
+    })
+  },
+  fetchGlobalCompatibility () {
+    return axios.get(`${ROOT_URL}/config`, defaultHeaders)
+  },
+  /**
+   * NONE, FULL, FORWARD, BACKWARD, FULL_TRANSITIVE, FORWARD_TRANSITIVE, BACKWARD_TRANSITIVE
+   */
+  updateCompatibilityForSubject (subjectName, compatibilityLevel) {
+    return axios.put(`${ROOT_URL}/config/${subjectName}`, {
+      headers: defaultHeaders.headers,
+      body: {
+        'compatibility': `\"${compatibilityLevel}\"`
+      }
+    })
+  },
+  fetchSubjectCompatibility (subjectName) {
+    return axios.get(`${ROOT_URL}/config/${subjectName}`, defaultHeaders)
   }
 }
