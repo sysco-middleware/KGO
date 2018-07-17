8887f421fba87148e32bb37e0d5a9350b8621f9c
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-07-17 18:20:41 +0200

    feat: export topic button

--- src/views/topics/Navigation.vue
+++ src/views/topics/Navigation.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <button class="btn">Export Topics</button>
+    <button v-if="selected" class="btn">Export Topic</button>
   </div>
 </template>
+
+<script>
+export default {
+  watch: {
+    '$route.params' () {
+      const {params} = this.$route
+      this.selected = params.topic
+    }
+  },
+  data () {
+    const {params} = this.$route
+    return {
+      selected: params.topic
+    }
+  }
+}
+</script>
