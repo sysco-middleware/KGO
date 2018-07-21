a63c61279d717377ca367b46937d9f5d9c93fc33
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-07-21 14:13:26 +0200

    feat: export consumed topic messages

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,24 @@
+/**
+ * Generate a GUID
+ * @return {String} generated GUID
+ */
 export function GUID () {
   function _p8 (s) {
     var p = (Math.random().toString(16) + '000000000').substr(2, 8)
     return s ? '-' + p.substr(0, 4) + '-' + p.substr(4, 4) : p
   }
   return _p8() + _p8(true) + _p8(true) + _p8()
+}
+
+/**
+ * Download the file from the given url with the given name.
+ * @param  {String} url   Location/URL of download
+ * @param  {String} name  Name of the document
+ */
+export function download (url, name) {
+  const link = document.createElement('a')
+  link.href = url
+  link.download = name
+  link.click()
+  link.remove()
 }
--- src/views/topics/Navigation.vue
+++ src/views/topics/Navigation.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <button v-if="selected" class="btn">Export Topic</button>
+    <button v-if="selected" @click="exportTopic()" class="btn">Export Topic</button>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+import * as utils from '@/lib/utils'
+
 export default {
+  computed: {
+    ...mapState('messages', [
+      'messages'
+    ]),
+  },
   watch: {
     '$route.params' () {
       const {params} = this.$route
@@ -16,6 +24,19 @@ export default {
     const {params} = this.$route
     return {
       selected: params.topic
+    }
+  },
+  methods: {
+    exportTopic () {
+      if (!this.selected) {
+        return
+      }
+
+      const messages = this.messages[this.selected] || []
+      const data = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(messages))}`
+      const name = `${this.selected}.json`
+
+      utils.download(data, name)
     }
   }
 }
