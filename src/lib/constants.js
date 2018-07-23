7e098e4a9b9091bed038a39e2a79c7b6d061d7e0
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-07-23 11:51:21 +0200

    feat: kafka proxy content type header

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,4 @@
 export const CONSUMER_FORMAT_ERROR = 40601
 export const KAFKA_GROUP_PREFIX = 'kafka-ui'
 export const LOCAL_STORAGE_PREFIX = 'kafka-ui'
+export const CONTENT_JSON_KAFKA = 'application/vnd.kafka.v2+json'
